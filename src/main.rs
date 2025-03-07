ab882870c60968487b9026e2902e44b81b014ea4
archzs <97004504+archzs@users.noreply.github.com>
2025-03-07 00:09:28 -0800

    Implement config file reading and writing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,94 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process;
+use std::io::Write;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    player_name: String,
+}
+
 fn main() {
+    let config_values: Config = load_config();
+    println!("Player name: {}", config_values.player_name);
     todo!();
+}
+
+fn load_config() -> Config {
+    // Check if config directory exists at $HOME/.config/lamp-drpc, create it if it does not.
+    let config_dir_path;
+    match env::home_dir() {
+        Some(path) => {
+            config_dir_path = path.to_str().unwrap().to_owned() + "/.config/lamp-drpc";
+        },
+        None => {
+            eprintln!("Error: Could not find home directory.");
+            process::exit(1);
+        },
+    }
+    match fs::exists(&config_dir_path) {
+        Ok(true) if Path::new(&config_dir_path.as_str()).is_dir() => {},
+        Ok(true) => { 
+            // Configuration directory exists, but is not a directory.
+            eprintln!("Error: file at {} is not a directory.", config_dir_path);
+            process::exit(1);
+        },
+        Ok(false) => {
+            // Configuration directory does not exist, create it now.
+            match fs::create_dir_all(&config_dir_path) {
+                Ok(_) => {},
+                Err(e) =>  {
+                    eprintln!("Error: {}", e);
+                    process::exit(1);
+                },
+            }
+        },
+        Err(e) => { 
+            eprintln!("Error: {}", e); 
+            process::exit(1); 
+        },
+    }
+    
+    // Check for configuration file. If it exists, read it, otherwise, create with default values.
+    let config_file_path = config_dir_path + "/lamp.toml";
+    match fs::exists(&config_file_path) {
+        Ok(true) => {
+            // Read existing configuration file.         
+            let toml_string = fs::read_to_string(config_file_path).ok().unwrap_or(String::from("player_name = \'cmus\'"));
+            match toml::from_str(toml_string.as_str()) {
+                Ok(config_values) => return config_values,
+                Err(e) => {
+                    eprintln!("Error: {}", e); 
+                    process::exit(1);
+                }
+            }
+        },
+        Ok(false) => {
+            // Configuration file does not exist, create it now and write default values to it.
+            let config_file = fs::OpenOptions::new()
+                                .read(false)
+                                .write(true)
+                                .create(true)
+                                .open(config_file_path);
+
+            // Write cmus as default player.
+            if config_file.is_ok() {
+                let _ = config_file.expect("Configuration file should exist and be accessible at this point.").write_all(b"player_name = \'cmus\'");
+            } else {
+                eprintln!("Error: {}", config_file.unwrap_err());
+                    process::exit(1);
+            }
+
+            let config_values = Config {
+                player_name: String::from("cmus"),
+            };
+            return config_values;
+        },
+        Err(e) => { 
+            eprintln!("Error: {}", e); 
+            process::exit(1); 
+        },
+    }
 }
