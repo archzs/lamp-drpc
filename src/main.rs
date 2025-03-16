6a041ca535db612a5f7495d4856659381f903179
archzs <97004504+archzs@users.noreply.github.com>
2025-03-15 20:55:37 -0700

    Create main loop with player status checking and initial delay

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,55 @@
 use std::env;
 use std::fs;
+use std::io::Write;
 use std::path::Path;
 use std::process;
-use std::io::Write;
+use std::thread;
+use std::time::Duration;
 use serde::Deserialize;
+use sysinfo::{Pid, ProcessStatus, ProcessesToUpdate, ProcessRefreshKind, RefreshKind, System};
 
 #[derive(Deserialize)]
 struct Config {
     player_name: String,
     va_individual_art: bool,
+    player_check_delay: u64,
 }
 
 fn main() {
     let config_values: Config = load_config();
-    println!("Player name: {}", config_values.player_name);
-    // Add error logging on errors
-    todo!();
+    let sleep_time: Duration = Duration::from_secs(config_values.player_check_delay);
+    
+    // TODO: Change player struct based on corresponding player_name module for different modular functions throughout main
+
+    // Wait player_check_delay number of seconds before checking that player is running
+    thread::sleep(sleep_time);
+
+    // Instantiate system instance with variable to track player status
+    let mut sys = System::new_with_specifics(RefreshKind::nothing().with_processes(ProcessRefreshKind::nothing()));
+    let mut player_status = ProcessStatus::Stop;
+
+    // Get PID of player process for checking process status
+    let mut player_pid = get_pid_by_proc_name(&sys, &config_values.player_name);
+
+    // Get status of player process by PID
+    player_status = get_status_by_pid(&sys, &player_pid);
+
+    // Begin main loop
+    while player_status != ProcessStatus::Stop {
+
+        // Refresh system processes
+        sys.refresh_processes_specifics(
+            ProcessesToUpdate::Some(&[player_pid]),
+            true,
+            ProcessRefreshKind::nothing(),
+        );
+
+        // Check player status, exit if None
+        let Some(player_process) = sys.process(Pid::from(player_pid)) else {
+            process::exit(0);
+        };
+        player_status = player_process.status();
+    }
 }
 
 fn load_config() -> Config {
@@ -80,11 +114,13 @@ fn load_config() -> Config {
                 - player_name is the name of the process to be tracked while running. Default is 'cmus'.
                 - va_individual_art indidcates whether or not tracks with "Various Artists" as the album artist
                   should have their album art processed individually. Default is true.
+                - player_check_delay becomes the amount of time to sleep before checking for the player running.
             */ 
             match config_file {
                 Ok(_) =>  {
                     let _ = write!(config_file.expect("Configuration file should exist and be accessible at this point."), "player_name = \'cmus\'\n\
-                                                                                                                            va_individual_art = true\n");
+                                                                                                                            va_individual_art = true\n\
+                                                                                                                            player_check_delay = 5\n");
                 },
                 Err(e) => {
                     log_error(&e.to_string().as_str());
@@ -95,6 +131,7 @@ fn load_config() -> Config {
             let config_values = Config {
                 player_name: String::from("cmus"),
                 va_individual_art: true,
+                player_check_delay: 5,
             };
             return config_values;
         },
@@ -131,6 +168,25 @@ fn log_error(e: &str) {
             eprintln!("Error: Could not find home directory.");
             process::exit(1);
         },
+    } 
+}
+
+fn get_pid_by_proc_name(sys: &System, proc_name: &String) -> sysinfo::Pid {
+    if let Some(possible_process) = sys.processes_by_exact_name(proc_name.as_ref()).next() {
+        return possible_process.pid();
     }
-    
+    else {
+        log_error(format!("The PID of target player {} could not be determined. The player may not be running or may have a different process name than provided in the configuration file.", proc_name).as_str());
+        process::exit(1);
+    }
+}
+
+fn get_status_by_pid(sys: &System, player_pid: &sysinfo::Pid) -> ProcessStatus {
+    if let Some(player_process) = sys.process(*player_pid) {
+        return player_process.status();
+    }
+    else {
+        log_error("The target PID could not be found. The player may no longer be running.");
+        process::exit(1);
+    }
 }
